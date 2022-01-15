505a8f8419ec5a3f7bfd47483e04d0ee184f33fc
Darthporkius <cholochullo@hotmail.com>
2022-01-15 13:07:26 +0100

    grid ready for testing, building the app

---
+++ js/memoria_app.js
@@ -0,0 +1,38 @@
+// Global functions
+// - make element
+// - select element
+
+let tileArray = [
+	{
+		name: "mama_papa",
+		picture: "../images/mama_papa.jpg"
+	},
+	{
+		name: "mama_lizzy",
+		picture: "../images/mama_lizzy.jpg"
+	},
+	{
+		name: "mama_evee",
+		picture: "../images/mama_evee.jpg" 
+	},
+	{
+		name: "guitar",
+		picture: "../images/guitar.jpg"
+	},
+	{
+		name: "guitar",
+		picture: "../images/guitar.jpg"
+	},
+	{
+		name: "mama_evee",
+		picture: "../images/mama_evee.jpg"
+	},
+	{
+		name: "mama_papa",
+		picture: "../images/mama_papa.jpg"
+	},
+	{
+		name: "mama_lizzy",
+		picture "../images/mama_lizzy.jpg"
+	}
+]
